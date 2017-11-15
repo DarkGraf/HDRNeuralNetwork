b189428c339d7503e6ab4ef7f1fd4a1b61c34040
Sergey Tolchev <stolchev@baltbet.ru>
2017-11-15 17:31:33 +0300

    Simple image.

--- HDRNeuralNetwork.WinApp/ViewModels/MainWindowModel.cs
+++ HDRNeuralNetwork.WinApp/ViewModels/MainWindowModel.cs
@@ -1,5 +1,7 @@
 ﻿using HDRNeuralNetwork.WinApp.Interfaces;
 using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace HDRNeuralNetwork.WinApp.ViewModels
 {
@@ -15,6 +17,30 @@ namespace HDRNeuralNetwork.WinApp.ViewModels
             }
 
             this.window = window;
+
+            CreateBitmapSource();
         }
+
+        private void CreateBitmapSource()
+        {
+            double dpi = 96;
+            int width = 128;
+            int height = 128;
+            byte[] pixelData = new byte[width * height];
+
+            for (int y = 0; y < height; ++y)
+            {
+                int yIndex = y * width;
+                for (int x = 0; x < width; ++x)
+                {
+                    pixelData[x + yIndex] = (byte)(x + y);
+                }
+            }
+
+            BitmapSource = BitmapSource.Create(width, height, dpi, dpi,
+                PixelFormats.Gray8, null, pixelData, width);
+        }
+
+        public BitmapSource BitmapSource { get; set; }
     }
 }
